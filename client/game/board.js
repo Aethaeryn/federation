7fe5f41727be32e956c53450db35873af857c57d
Michael Babich <michaelbabich7@gmail.com>
2011-12-20 19:27:25 -0500

    Rewriting board.js to treat Board as an object.

--- client/game/board.js
+++ client/game/board.js
@@ -14,97 +14,109 @@
 //    You should have received a copy of the GNU General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function getCoords(x_shift, y_shift) {
-    var coords = [[14, 0], [43, 0], [57, 27], [43, 54], [14, 54], [0, 27]];
+function Board () {
+    this.moved = false;
+    this.x = 0;
+    this.y = 0;
 
-    for (var i = 0; i < coords.length; i++) {
-        coords[i][0] += x_shift;
-        coords[i][1] += y_shift;
-    }
+    this.board = function() {
+        const X = 0;
+        const Y = 1;
 
-    return coords;
-}
+        const HEX_SIZE = [57, 54];
+        const HEX_OFFSET = [43, 27];
 
-function getCoordCenter(hex_coords) {
-    return [(hex_coords[0][0] + hex_coords[1][0]) / 2, hex_coords[2][1]];
-}
+        var hex_grid = [40, 40];
 
-function draw(coords) {
-    var board = document.getElementById('board').getContext('2d');
+        // Half of the total hex width, half of the middle (odd) hex width, and an extra 14 gives the max.
+        this.x_max = (hex_grid[X] / 2) * HEX_SIZE[X] + (hex_grid[X] / 2) * 29 + 14;
 
-    board.strokeStyle = '#aaaaaa';
-    board.lineWidth = 1;
-    board.beginPath();
+        // The total hex height, plus the y offset, gives the max.
+        this.y_max = (HEX_SIZE[Y] * hex_grid[Y] + HEX_OFFSET[Y]);
 
-    board.moveTo(coords[0][0], coords[0][1]);
+        /* fixme: code to center the board on start
+           if (this.moved == false) {
+           this.x = - this.x_max / 2;
+           this.y = - this.y_max / 2;
+           } */
 
-    for (var i = 1; i < coords.length; i++) {
-        board.lineTo(coords[i][0], coords[i][1]);        
-    }
+        var off_x = this.x;
+        var off_y = this.y;
 
-    board.closePath();
-    board.stroke();
-}
+        setSize("#333333");
 
-function board() {
-    const X = 0;
-    const Y = 1;
+        this.hexagons = [];
 
-    const HEX_SIZE = [57, 54];
-    const HEX_OFFSET = [43, 27];
+        for (var i = 0; i < hex_grid[Y]; i++) {
+            x = 0 + off_x;
+            y = i * HEX_SIZE[Y] + off_y;
 
-    var hex_grid = [40, 40];
+            for (var j = 0; j < hex_grid[X]; j++) {
+                this.hexagons.push(this.getCoords(x, y));
+                x += HEX_OFFSET[X];
 
-    // Half of the total hex width, half of the middle (odd) hex width, and an extra 14 gives the max.
-    boardimg.x_max = (hex_grid[X] / 2) * HEX_SIZE[X] + (hex_grid[X] / 2) * 29 + 14;
-
-    // The total hex height, plus the y offset, gives the max.
-    boardimg.y_max = (HEX_SIZE[Y] * hex_grid[Y] + HEX_OFFSET[Y]);
-
-    /* fixme: code to center the board on start
-    if (boardimg.moved == false) {
-        boardimg.x = - boardimg.x_max / 2;
-        boardimg.y = - boardimg.y_max / 2;
-    } */
-
-    var off_x = boardimg.x;
-    var off_y = boardimg.y;
-
-    var hexagons = [];
-
-    setSize("#333333");
-
-    for (var i = 0; i < hex_grid[Y]; i++) {
-        x = 0 + off_x;
-        y = i * HEX_SIZE[Y] + off_y;
-
-        for (var j = 0; j < hex_grid[X]; j++) {
-            hexagons.push(getCoords(x, y));
-            x += HEX_OFFSET[X];
-
-            if (j % 2) {
-                y -= HEX_OFFSET[Y];
-            } else {
-                y += HEX_OFFSET[Y];
+                if (j % 2) {
+                    y -= HEX_OFFSET[Y];
+                } else {
+                    y += HEX_OFFSET[Y];
+                }
             }
+        }
+
+        this.grid();
+    }
+
+    this.grid = function () {
+        for (var i = 0; i < this.hexagons.length; i++) {
+            this.drawGrid(this.hexagons[i]);
         }
     }
 
-    for (var i = 0; i < hexagons.length; i++) {
-        draw(hexagons[i]);
+    this.getCoords = function (x_shift, y_shift) {
+        var coords = [[14, 0], [43, 0], [57, 27], [43, 54], [14, 54], [0, 27]];
+
+        for (var i = 0; i < coords.length; i++) {
+            coords[i][0] += x_shift;
+            coords[i][1] += y_shift;
+        }
+
+        return coords;
+    }
+
+    this.getCoordCenter = function (hex_coords) {
+        return [(hex_coords[0][0] + hex_coords[1][0]) / 2, hex_coords[2][1]];
+    }
+
+    this.drawGrid = function (coords) {
+        var board_canvas = document.getElementById('board').getContext('2d');
+
+        board_canvas.strokeStyle = '#aaaaaa';
+        board_canvas.lineWidth = 1;
+        board_canvas.beginPath();
+
+        board_canvas.moveTo(coords[0][0], coords[0][1]);
+
+        for (var i = 1; i < coords.length; i++) {
+            board_canvas.lineTo(coords[i][0], coords[i][1]);        
+        }
+
+        board_canvas.closePath();
+        board_canvas.stroke();
     }
 }
+
+var board = new Board();
 
 function setSize(color) {
     var x_pixels = window.innerWidth;
     var y_pixels = window.innerHeight;
 
-    var board = document.getElementById('board');
-    board.setAttribute("width", x_pixels - 260);
-    board.setAttribute("height", y_pixels - 88);
+    var board_canvas = document.getElementById('board');
+    board_canvas.setAttribute("width", x_pixels - 260);
+    board_canvas.setAttribute("height", y_pixels - 88);
 
-    boardimg.y_height = y_pixels - 88;
-    boardimg.x_height = x_pixels - 260;
+    board.y_height = y_pixels - 88;
+    board.x_height = x_pixels - 260;
 
     var sidebar = document.getElementById('sidebar');
     sidebar.setAttribute("width", 220);
@@ -115,9 +127,7 @@ function setSize(color) {
     side_canvas.fillRect(0, 0, 220, y_pixels - 88);
 
     side_canvas.fillStyle = color;
-
     side_canvas.fillRect(10, 10, 200, 150);
-
     side_canvas.fillRect(10, 165, 50, 50);
 
     var footer = document.getElementById('footer');
@@ -140,73 +150,71 @@ function setSize(color) {
 }
 
 window.onresize = function(event) {
-    board();
+    board.board();
+}
+
+window.onload = function(event) {
+    board.board();
 }
 
 function keyActions(event) {
     const SCROLL = 20;
 
-    boardimg.moved = true;
+    board.moved = true;
 
     // Left scrolls left.
     if (event.keyCode == 37) {
-        if (boardimg.x + SCROLL <= 0) {
-            boardimg.x += SCROLL;
+        if (board.x + SCROLL <= 0) {
+            board.x += SCROLL;
         }
     }
 
     // Up scrolls up.
     if (event.keyCode == 38) {
-        if (boardimg.y + SCROLL <= 0) {
-            boardimg.y += SCROLL;
+        if (board.y + SCROLL <= 0) {
+            board.y += SCROLL;
         }
     }
 
     // Right scrolls right.
     if (event.keyCode == 39) {
-        if (boardimg.x - boardimg.x_height >= - boardimg.x_max) {
-            boardimg.x -= SCROLL;
+        if (board.x - board.x_height >= - board.x_max) {
+            board.x -= SCROLL;
         }
     }
 
     // Down scrolls down.
     if (event.keyCode == 40) {
-        if (boardimg.y - boardimg.y_height >= - boardimg.y_max) {
-            boardimg.y -= SCROLL;
+        if (board.y - board.y_height >= - board.y_max) {
+            board.y -= SCROLL;
         }
     }
 
-    board();
+    board.board();
 
-/*    switch (event.keyCode) {
+    /*
+    switch (event.keyCode) {
     case 71: // 'g'
         break;
     }
-*/
+    */
 }
 
 // Accurately captures location of mouse on board canvas.
 function mouseMove(event) {
-    var x = event.clientX - 10 - boardimg.x;
-    var y = event.clientY - 45 - boardimg.y;
+    var x = event.clientX - 10 - board.x;
+    var y = event.clientY - 45 - board.y;
 
-    if (x < 0 || x > boardimg.x_height) {
+    if (x < 0 || x > board.x_height) {
         x = false;
     }
 
-    if (y < 0 || y > boardimg.y_height) {
+    if (y < 0 || y > board.y_height) {
         y = false;
     }
 
-    alert(x + ', ' + y);
+    // alert(x + ', ' + y);
 }
 
 window.addEventListener('keydown', keyActions, true);
 document.addEventListener('mousemove', mouseMove, true)
-
-boardimg = new Object();
-boardimg.moved = false;
-boardimg.x = 0;
-boardimg.y = 0;
-
-board();
