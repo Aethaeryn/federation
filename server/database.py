2dd0127e362f6bed88439e64073eb03cd9583913
Michael Babich <michaelbabich7@gmail.com>
2012-04-03 18:52:43 -0400

    Slight clean up of existing db code.

--- server/database.py
+++ server/database.py
@@ -14,11 +14,13 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from server import app
+from server import app, data
 from sqlalchemy import create_engine, Column, Integer, Boolean, String
+from sqlalchemy.types import DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+#### TODO: Put in an actual location and turn off echo
 engine = create_engine('sqlite:///:memory:', echo=True)
 
 Base = declarative_base()
@@ -31,15 +33,16 @@ class Alliance(Base):
     id          = Column(Integer, primary_key=True)
     name        = Column(String(80), unique=True)
     founder     = Column(String(80))
-    # date of founding
+    date        = Column(DateTime())
     cash        = Column(Integer)
     tax_rate    = Column(Integer)
     shared_view = Column(Boolean)
-    # store leaders/founder/ranks/roles/etc.
+    #### store leaders/ranks/roles/etc.
 
     def __init__(self, name, founder):
         self.name    = name
         self.founder = founder
+        self.date    = data.Time.get()
 
     def __repr__(self):
         return '<Alliance %s>' % (self.name)
@@ -51,31 +54,43 @@ class Player(Base):
     username   = Column(String(80), unique=True)
     game_name  = Column(String(80))
     email      = Column(String(80))
-    # join date
+    date       = Column(DateTime())
     cash       = Column(Integer)
     income     = Column(Integer)
     research   = Column(Integer)
     federation = Column(String(80))
-    # count fleets, territories, ships
-    # associate ownership with player
-    # associate with membership in the actual alliance
+    #### count fleets, territories, ships
+    #### associate ownership with player
+    #### associate with membership in the actual alliance
 
-    # __init__, __repr__
+    def __init__(self, username, game_name, email):
+        self.username  = username
+        self.game_name = game_name
+        self.email     = email
+        self.date      = date.Time.get()
+
+    def __repr__(self):
+        return '<Player %s (%s)>' % (self.game_name, self.username)
 
 class Fleet(Base):
     __tablename__ = 'fleet'
 
-    # hold ships
-    # hold players
-    # count ships/players
+    #### hold ships
+    #### hold players
+    #### count ships/players
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80))
+    id         = Column(Integer, primary_key=True)
+    name       = Column(String(80))
     federation = Column(String(80))
-    commander = Column(String(80))
-    deputy = Column(String(80))
+    commander  = Column(String(80))
+    deputy     = Column(String(80))
 
-    # __init__, __repr__
+    def __init__(self, name, commander):
+        self.name = name
+        self.commander = commander
+
+    def __repr__(self):
+        return '<Fleet %s %s (%s)>' % (self.id, self.name, self.commander)
 
 Base.metadata.create_all(engine)
 
@@ -83,4 +98,3 @@ foo = Alliance("me", "you")
 session = Session()
 session.add(foo)
 session.commit()
-
