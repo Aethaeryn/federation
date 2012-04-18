a810aea03753f7c5f81a4b815db7bca91b19e809
Michael Babich <michaelbabich7@gmail.com>
2012-04-18 00:59:00 -0400

    Putting repeated code into their own functions.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2011, 2012 Michael Babich
 # See LICENSE.txt or http://www.opensource.org/licenses/mit-license.php
 
-import execjs, requests
+import execjs, requests, io
 from os import path, listdir, mkdir
 
 def location(target):
@@ -9,19 +9,23 @@ def location(target):
 
 class CoffeeScript():
     def __init__(self, filename):
-        self.file     = filename
-        source_file   = open(self.file, 'r')
-        self.source   = source_file.read()
+        self.source   = self._get_source(filename)
         self.compiled = execjs.get().compile(self.source)
 
+    def _get_source(self, filename):
+        source_file = io.open(filename, 'r', encoding='utf8')
+        source      = source_file.read()
         source_file.close()
+        return source
 
     def compile(self, filename):
-        source_file = open(filename, 'r')
-        source      = source_file.read()
-
-        source_file.close()
+        source = self._get_source(filename)
         return self.compiled.call('CoffeeScript.compile', source)
+
+def write_file(destination, content):
+    destination = io.open(destination, 'w', encoding='utf8')
+    destination.write(content)
+    destination.close()
 
 def get_libraries(directory):
     js_lib = {'jquery.js'        : 'http://code.jquery.com/jquery-1.7.2.min.js',
@@ -31,22 +35,16 @@ def get_libraries(directory):
         if library not in listdir(directory):
             downloaded  = requests.get(js_lib[library])
             destination = "%s%s" % (directory, library)
-            destination = open(destination, 'w')
             content     = downloaded.content
-
-            destination.write(content)
-            destination.close()
+            write_file(destination, content)
 
 def compile_coffee(coffee_dir, script_dir):
     coffee = CoffeeScript(script_dir + 'coffee-script.js')
 
     for filename in listdir(coffee_dir):
         js_filename = '%s%s.js' % (script_dir, filename.split('.')[0])
-        destination = open(js_filename, 'w')
         compiled    = coffee.compile(coffee_dir + filename)
-
-        destination.write(compiled)
-        destination.close()
+        write_file(js_filename, compiled)
 
 def main():
     coffee_dir = location('src/')
@@ -57,7 +55,6 @@ def main():
         mkdir(script_dir)
 
     get_libraries(script_dir)
-
     compile_coffee(coffee_dir, script_dir)
 
 if __name__ == '__main__':
