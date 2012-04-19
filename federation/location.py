3394952d9d9c98333bb14859ac0737fe110156a0
Michael Babich <michaelbabich7@gmail.com>
2012-04-19 15:37:08 -0400

    Turning a class into two functions.

--- federation/location.py
+++ federation/location.py
@@ -1,62 +1,62 @@
-# Copyright (c) 2011, 2012 Michael Babich
-# See LICENSE.txt or http://www.opensource.org/licenses/mit-license.php
+'''Provides certain algorithms for hex board calculations.
 
-# Provides certain algorithms for hex board calculations.
-# Takes in location tuples such as (0, 0)
-class Location:
-    # Calculates distance on a hex board.
-    @classmethod
-    def distance(self, start_location, end_location):
-        horizontal = abs(start_location[0] - end_location[0])
+Copyright (c) 2011, 2012 Michael Babich
+See LICENSE.txt or http://www.opensource.org/licenses/mit-license.php
+'''
+def distance(start_location, end_location):
+    '''Calculates the difference between two points on a hex board
+    expressed in (x, y) coordinates.
+    '''
+    horizontal = abs(start_location[0] - end_location[0])
 
-        penalty = 0
+    penalty = 0
 
-        if (((start_location[0] % 2) and not (end_location[0] % 2) and start_location[1] < end_location[1])
-            or (end_location[0] % 2) and not (start_location[0] % 2) and end_location[1] < start_location[1]):
-            penalty = 1
+    if (((start_location[0] % 2) and not (end_location[0] % 2) and start_location[1] < end_location[1])
+        or (end_location[0] % 2) and not (start_location[0] % 2) and end_location[1] < start_location[1]):
+        penalty = 1
 
-        alt_distance = abs(start_location[1] - end_location[1]) + penalty + (horizontal / 2)
+    alt_distance = abs(start_location[1] - end_location[1]) + penalty + (horizontal / 2)
 
-        return max(horizontal, alt_distance)
+    return max(horizontal, alt_distance)
 
-    # Returns a set of tuple (x, y) coordinates in the radius range.
-    @classmethod
-    def radius(self, location, radius):
-        radius_set = set([])
+def radius(self, location, radius):
+    '''Returns a set of tuple (x, y) coordinates in the radius range.
+    '''
+    radius_set = set([])
 
-        # Case 1: The radius is even.
-        if not radius % 2:
+    # Case 1: The radius is even.
+    if not radius % 2:
 
-            # Generates the left side of the hex range.
-            for i in range(-radius + (radius / 2), radius - (radius / 2) + 1):
-                radius_set.add((location[0] - radius, location[1] + i))
+        # Generates the left side of the hex range.
+        for i in range(-radius + (radius / 2), radius - (radius / 2) + 1):
+            radius_set.add((location[0] - radius, location[1] + i))
 
-            # Generates the right side of the hex range
-            for i in range(-radius + (radius / 2), radius - (radius / 2) + 1):
-                radius_set.add((location[0] + radius, location[1] + i))
+        # Generates the right side of the hex range
+        for i in range(-radius + (radius / 2), radius - (radius / 2) + 1):
+            radius_set.add((location[0] + radius, location[1] + i))
 
-            # Connects the corners on the other sides.
-            y1  = location[1] - radius
-            y2  = location[1] + radius
-            x1  = location[0]
-            x2  = location[0]
+        # Connects the corners on the other sides.
+        y1  = location[1] - radius
+        y2  = location[1] + radius
+        x1  = location[0]
+        x2  = location[0]
 
-            for i in range(radius):
-                radius_set.add((x1, y1))
-                radius_set.add((x2, y1))
-                radius_set.add((x1, y2))
-                radius_set.add((x2, y2))
+        for i in range(radius):
+            radius_set.add((x1, y1))
+            radius_set.add((x2, y1))
+            radius_set.add((x1, y2))
+            radius_set.add((x2, y2))
 
-                # Odds walk the hexes a certain way.
-                if x1 % 2:
-                    x1 -= 1
-                    x2 += 1
-                    y2 -= 1
+            # Odds walk the hexes a certain way.
+            if x1 % 2:
+                x1 -= 1
+                x2 += 1
+                y2 -= 1
 
-                # Evens walk the hexes another way.
-                else:
-                    x1 -= 1
-                    x2 += 1
-                    y1 += 1
+            # Evens walk the hexes another way.
+            else:
+                x1 -= 1
+                x2 += 1
+                y1 += 1
 
-        return radius_set
+    return radius_set
