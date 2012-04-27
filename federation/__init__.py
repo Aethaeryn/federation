08ed67c3b5100ca5ebc3308f20a40030a264a563
Michael Babich <michaelbabich7@gmail.com>
2012-04-26 21:45:05 -0400

    Returning to the old style, using decorators.

--- federation/__init__.py
+++ federation/__init__.py
@@ -8,23 +8,10 @@ The server uses JSON as a client API. It also serves http pages that
 can be accessed directly by a modern browser with JavaScript and
 canvas support.
 '''
-from federation import public, game
 from flask import Blueprint
 
-class Federation():
-    website = [['/'                       , 'game_index',  public.game_index],
-               ['/data/'                  , 'data',        public.data_folder],
-               ['/data/environment'       , 'environment', public.environment],
-               ['/data/player/'           , 'players',     public.players],
-               ['/data/player/<username>' , 'player',      public.player],
-               ['/data/secret'            , 'secret',      public.secret]]
+app = Blueprint('federation', __name__, static_folder='static', url_prefix='/federation')
 
-    def __init__(self):
-        game.start()
+from federation import public, game
 
-app = Blueprint('fedgame', __name__, static_folder='static', url_prefix='/federation')
-
-foo = Federation()
-
-for page in foo.website:
-    app.add_url_rule(*page)
+game.start()
--- federation/public.py
+++ federation/public.py
@@ -4,9 +4,10 @@
 '''Serves the public data API json and game client html using flask
 for dynamic rendering of the content.
 '''
-from federation import game
+from federation import game, app
 from avenue.api import *
 
+@app.route('/')
 def game_index():
     '''Creates an html page that uses javascript with canvas to format
     the main game board. This serves as a client built into the server
@@ -25,6 +26,7 @@ def game_index():
     return make_page(body=html, javascript=scripts, head=header,
                      style='../static/dark-plain')
 
+@app.route('/data/')
 def data_folder():
     '''Tells the client which pages to look for in the data directory
     for JSON information to parse.
@@ -38,22 +40,26 @@ def data_folder():
     return make_json(available)
 
 #### TODO: This is currently broken
+@app.route('/data/environment')
 def environment():
     '''Displays the public data from federation/data/environment in a
     way that the clients can parse using JSON.
     '''
     return make_json(game.env.convert())
 
+@app.route('/data/player/')
 def players():
     '''Displays the username and IDs of all the players in the game.
     '''
     return make_json(game.get_all_players())
 
+@app.route('/data/player/<username>')
 def player(username):
     '''Displays the public stats of any given user.
     '''
     return make_json(game.get_player(username))
 
+@app.route('/data/secret')
 def secret():
     '''This is a temporary test to show data to an authenticated user.
     '''
